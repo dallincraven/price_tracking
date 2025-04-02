4629faccd27a84438630719ca81c73d8d60cf9a6
Dallin Craven <dallincraven@gmail.com>
2025-04-02 15:17:26 -0600

    add features process_products and get_response

--- track_price.py
+++ track_price.py
@@ -8,3 +8,12 @@ PRODUCT_URL_CSV = "products.csv"
 SAVE_TO_CSV = True
 PRICES_CSV = "prices.csv"
 SEND_MAIL = True
+
+def process_products(df):
+	for product in df.to_dict("records"):
+		# product["url"] is the URL
+
+def get_response(url):
+	response = requests.get(url)
+	return response.text
+
